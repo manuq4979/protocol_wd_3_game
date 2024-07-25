360a19e3c940012140f778492ad14f29f02873cd
manuq4979 <76844037+manuq4979@users.noreply.github.com>
2024-07-25 14:09:12 +0300

    Update task.py

--- task.py
+++ task.py
@@ -497,7 +497,7 @@ def get_pages(task_list, chunk_size=3):
     for i in range(len(new_task_list)):
         if len(new_task_list[i]) == 0:
             new_task_list.pop(i)
-    print(new_task_list)
+    # print(new_task_list)
     
     return new_task_list
 
