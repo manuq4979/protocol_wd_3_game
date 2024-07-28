664b4f9bf50616b66077e48682e4e80fa538232a
manuq4979 <76844037+manuq4979@users.noreply.github.com>
2024-07-28 03:06:58 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,51 @@
-# Пример строки:
-# "name_NI=1_RM=0_OS=1_PoF=0_AI=0_CP=0_UI=1_CP2=0_W=0_R=0+S;gun_damage=100_charge=100;gun_recharge=100+AAD;bot_HP=100_armor=200_damage=100_strong=3_critical-dmg=45_drop-trophy:smart-card=write=True"
-def decoding_of_characteristics(char_line):
-        chars = char_line.split("+")
-        
-        base_char = chars[0]
-        storage =  chars[1]
-        access_algorithm_dict = chars[2]
-        
-        base_char = base_char.split("_")
-        name = base_char[0]
-        base_char.remove(name)
-        base_char_res = []
-        base_char_res.append(name)
-        for char in base_char:
-            char = char.split("=")
-            base_char_res.append(char)
-        
-        storage = storage.split(";")
-        
-        access_algorithm_dict = access_algorithm_dict.split(";")
-        
-        return [base_char_res, storage, access_algorithm_dict]
-        
-        
-res = decoding_of_characteristics("name_NI=1_RM=0_OS=1_PoF=0_AI=0_CP=0_UI=1_CP2=0_W=0_R=0+S;gun_damage=100_charge=100;gun_recharge=100+AAD;bot_HP=100_armor=200_damage=100_strong=3_critical-dmg=45_drop-trophy:smart-card=write=True")
-print(res)
+name = input("Укажите имя для системы> ")
+network_interface = input("Есть ли доступ в интернет> ")
+remote_access = input("Есть ли удаленный доступ> ")
+operation_system = input("Есть ли ОС> ")
+possibility_of_flashing = input("Есть ли возможность поошивки> ")
+artificial_intelligence = input("Есть ли AI>")
+connection_port = input("Есть ли физические порты доступа> ")
+user_interface = input("Есть ли UI(нужен для удаленного доступа)> ")
+control_panel = input("Есть ли панель управления(нужна для физического доступа)> ")
+write = input("Есть ли доступ на запись> ")
+read = input("Есть ли доступ на чтение> ")
+
+i = 0
+while True:
+        print("Добавлено: "+str(i))
+        print("0. Готово")
+        res = input("Цель с доступом к системе> ")
+        if res == "0":
+                access_algorithm_dict.pop(-1) # удаляем лишнию ;
+                break
+        access_algorithm_dict += res + ";"
+        i += 1
+i = 0
+while True:
+        print("Добавлено: "+str(i))
+        print("0. Готово")
+        rea = input("Добавьте tool_id> ")
+        if res == "0":
+                storage.pop(-1) # удаляем лишнию ;
+                break
+        storage += res + ";"
+        i += 1
+
+SE_ID = (name+"_"+
+         "NI="+network_interface+
+         "RA="+remote_access+
+         "OS="+operation_system+
+         "PoF="+possibility_of_flashing+
+         "AI="+artificial_intelligence+
+         "CP="+connection_port+
+         "UI="+user_interface+
+         "CP2="+control_panel+
+         "W="+write+
+         "R="+read+"+"+
+         "S;"+storage+"+"+
+         "AAD;"+access_algorithm_dict)
+
+print(SE_ID)
+         
+ 
+
