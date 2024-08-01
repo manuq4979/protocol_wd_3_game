e3990e4d96f4d2a0adea4867200d86ae3041fe07
manuq4979 <76844037+manuq4979@users.noreply.github.com>
2024-08-01 13:57:45 +0300

    Update se_applications.py

--- se_applications.py
+++ se_applications.py
@@ -4,7 +4,14 @@
 
 ###
 
-
+percent = 40
+def hacked_calculate_drop_trophy(npc):
+    r = random.randint(1, 100)
+    print("Трофей: "+str(r))
+    if r <= percent:
+        return npc.drop_trophy
+    return 0
+    
 # APP NUMBER 1
 def hack_trophy(prof):
     bots_id = [["two-wheeled-bot", 70], ["spider-bot", 100]]
@@ -12,7 +19,9 @@ def hack_trophy(prof):
         print("\n#######################################################\n")
         print("\033[32m{}".format("[INFO]: ")+"\033[0m{}".format("Приложение позволяет взламывать вам противника с помощью ботов!\n"))
         print("Hack Menu: ----------------------")
-        print("[1]: Подключиться к боту.")
+        print("[1]: Выполнить взлом.")
+        print("[2]: Отменить взлом.")
+        print("[3]: Список поддерживаемых ботов.")
         print("[0]: Выход.")
         print("\n#######################################################\n")
     
@@ -29,10 +38,27 @@ def hack_trophy(prof):
                 for bot_id in bots_id:
                     if tool_id == bot_id[0]:
                         my_bota.append(bot_id)
+            if len(my_bots) == 0:
+                print("\033[31m{}".format("[ERROR]: ")+"\033[0m{}".format("Требуется наличие бота в инвентаре!"))
+                input("\033[32m{}".format("[INFO]: ")+"\033[0m{}".format("Нажмите <enter> чтобы продолжить..."))
+                continue
             values = []
             for bot_id in my_bots:
                 values.append(bot_id[1])
             max_percent = max(values)
+            
+            from player_profile import calculate_drop_trophy
+            global percent
+            percent = max_percent
+            calculate_drop_trophy = hacked_calculate_drop_trophy
+            print("\033[32m{}".format("[INFO]: ")+"\033[0m{}".format("Вероятность выпадения вместо 40 - "+str(percent)+"%."))
+            input("\033[32m{}".format("[INFO]: ")+"\033[0m{}".format("Нажмите <enter> чтобы продолжить..."))
+        if command == "2":
+            from player_profile import calculate_drop_trophy
+            global percent
+            percent = 40
+            calculate_drop_trophy = calculate_drop_trophy # как вернуть адресс прежней функции??
+            
                 
 
 
