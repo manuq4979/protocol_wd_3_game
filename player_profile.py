8b4b9806bb0d2aa8929c14e1317324c9c1e3ad0a
manuq4979 <76844037+manuq4979@users.noreply.github.com>
2024-08-01 16:11:42 +0300

    Update player_profile.py

--- player_profile.py
+++ player_profile.py
@@ -451,7 +451,7 @@ def find_recharge_for_tool_id(prof, finding_tool_id, array_tool=[], i=0):
         # Инструмент может иметь рандомное значение charge - заряда, из-за чего обычный поиск может не дать результата, поэтому искать инструмент буду по имени.
         if tool_recharge_id.find("recharge") == -1:
             continue
-        tool_recharge_id = tool_recharge_id.split(":")[1].split("_") # это патроны
+        tool_recharge_id = tool_recharge_id.split(":")[1].split("_")[0] # это патроны
         print(str(finding_tool_id) + " == "+str(tool_recharge_id))
         # строка patron_recharge:1911-pistols_damage=90_charge=100
         # станет ['recharge', '1911-pistols_damage=90_charge=0'], поэтому и [1], если именеи patron - не окажется, то индексом должно быть [0]!
