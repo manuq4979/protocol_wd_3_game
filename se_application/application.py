1dba1645abdebfa2e90c27c0cecd5ba59904ca62
manuq4979 <76844037+manuq4979@users.noreply.github.com>
2024-08-01 13:03:59 +0300

    Update application.py

--- se_application/application.py
+++ se_application/application.py
@@ -1,3 +1,21 @@
+###
+
+#    Тут приложения в виде обычных def:
+
+###
+
+
+
+
+
+
+
+###
+
+#    Ниже код для отображения приложений:
+
+###
+
 applications_list = [] # [[name, app_address], ...]
 
 def get_applications_and_print():
@@ -6,12 +24,19 @@ def get_applications_and_print():
         app = applications_list[i]
         print("["+str(i)+"]: "+ app[0]+".")
 
+# Вернет index или False с текстом ошибки в случае ошибки ввода:
 def check_input(input_text):
     if input_text.isdigit() == False:
         print("\033[31m{}".format("[ERROR]: ")+"\033[0m{}".format("Допустимы лишь числовые значения!"))
         input("\033[32m{}".format("[INFO]: ")+"\033[0m{}".format("Нажмите <enter> чтобы продолжить..."))
-
         return False
+    else:
+        index = int(input_text) - 1
+        if len(applications_list) < index:
+            print("\033[31m{}".format("[ERROR]: ")+"\033[0m{}".format("Данного пункта меню не существует!"))
+            input("\033[32m{}".format("[INFO]: ")+"\033[0m{}".format("Нажмите <enter> чтобы продолжить..."))
+            return False
+        return index
 
 def application_menu():
     print("\n#######################################################\n")
@@ -23,7 +48,7 @@ def application_menu():
     index = input("APP-MENU:~# ")
     if index == "0":
         return
+    index = check_input(index)
     
     
-    index = int(index) - 1
     applications_list[index][1]()
