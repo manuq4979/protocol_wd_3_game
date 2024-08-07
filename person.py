508b37503fb561153015edcc61a1ce37b110232f
manuq4979 <76844037+manuq4979@users.noreply.github.com>
2024-08-08 01:33:37 +0300

    Update person.py

--- person.py
+++ person.py
@@ -70,7 +70,7 @@ class NPC:
         new_dt = []
         print(drop_trophy[1])
         for i in range(len(drop_trophy[1])):
-            if drop_trophy[1][i].find("recharge"):
+            if drop_trophy[1][i].find("recharge") != -1:
                 print(i)
                 new_tool_id = drop_trophy[1][i] + ":" + drop_trophy[1][i+1]
                 new_dt.append(new_tool_id)
