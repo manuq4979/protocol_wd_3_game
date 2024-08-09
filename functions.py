f1848bc66592e690b99876b7c8d820f701b0488f
manuq4979 <76844037+manuq4979@users.noreply.github.com>
2024-08-10 00:33:59 +0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -293,7 +293,10 @@ def check_relevance_task():
         '''
         start_date = task.get_start_date()
         if task.check_time(start_date) != 1: # если сегодня не дата начала задания, то:
+            task.set_status("No active")     # сделать задние не активным и
             continue                         # то задание не проверять на просроченное!
+        else:                                # иначе:
+            task.set_status("No active")     # задание сделать активным и проверить на просроченность:
         if task.check_day_end() == True:
             
             if task.get_status() == "Active":
