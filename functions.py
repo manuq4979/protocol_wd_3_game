f51abddb145f0f83fa5b8bf75fc73d5f2fe559a6
manuq4979 <76844037+manuq4979@users.noreply.github.com>
2024-08-14 01:36:31 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -277,8 +277,10 @@ def get_inventory():
         slots          = prof.get_slots()
         slots_occupied = prof.get_slots_occupied()
         print("\033[32m{}".format("\nЭкиперованно")+"\033[0m{}".format("(")+str(slots_occupied)+" из "+str(slots)+"):")
-        for tool in keep_tool:
-            print("\033[34m{}".format("[Инструмент]: ")+"\033[0m{}".format(tool))
+        index = 0
+	for tool in keep_tool:
+            index += 1
+            print("\033[34m{}".format("[Инструмент]: ")+"\033[0m{}".format("["+str(index)+"]: "+tool))
         print("\nMenu: ----------------------")
         print("1. Экипировать")
         print("2. Снять предмет")
