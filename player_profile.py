93b30989c686f6101569365021a0924f37966d32
manuq4979 <76844037+manuq4979@users.noreply.github.com>
2024-08-20 15:04:09 +0400

    Update player_profile.py

--- player_profile.py
+++ player_profile.py
@@ -346,7 +346,7 @@ class Profile:
         my_characteristics = json.loads(json_string)
             
         self.set_new_characteristics(my_characteristics=my_characteristics, reset=True)
-        print("\033[32m{}".format("[INFO]: ")+"\033[0m{}".format("Возврат эарактеристик успешно завершен!\n\n\n"))
+        print("\033[32m{}".format("[INFO]: ")+"\033[0m{}".format("Возврат характеристик успешно завершен!\n\n\n"))
         input("\033[32m{}".format("[INFO]: ")+"\033[0m{}".format("Нажмите <enter> чтобы продолжить..."))
 
     # "gun_damage=200_strong=2" == ['gun', ['damage', '200'], ['strong', '2']]
