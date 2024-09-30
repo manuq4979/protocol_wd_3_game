28b1e08b9ebcd05c1e3f6970f10f92b1f7ebd25d
manuq4979 <76844037+manuq4979@users.noreply.github.com>
2024-09-30 21:14:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def print_NPC():
 
 
 while(True):
-    print("серия: "+str(task.get_series_point()))
+    print("серия: "+str(habit_task_dict.get(1).get_series_point()))
     check_time_to_disable_camouflage() # из модуля hacker_apps, как правило используется на пользу противнику
     if menu_line == True:
         print("\n#######################################################\n")
