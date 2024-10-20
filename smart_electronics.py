45016b47bef3476ab579a961d72cf0ceecf6fe76
manuq4979 <76844037+manuq4979@users.noreply.github.com>
2024-10-20 03:23:54 +0300

    Update smart_electronics.py

--- smart_electronics.py
+++ smart_electronics.py
@@ -456,7 +456,8 @@ def set_SE():
     SE_ID = input("SE_ID> ")
     SE_ID = SE_ID.replace(" ", "")
     if SE_ID == "":
-        print("\033[33m{}".format("[WARNING]: ")+"\033[0m{}".format("Операция отменена!"))
+        print("\033[33m{}".format("[WARNING]: ")+"\033[0m{}".format("Операция отменена!\n\n\n\n"))
+        input("\033[32m{}".format("[INFO]: ")+"\033[0m{}".format("Нажмите <enter> чтобы продолжить..."))
         return
     apply_to_characteristics_SE(SE_ID)
     print("\n" * 100) # очищаем экран консоли
