37c138b5cc609441811822e393152f0b1fbf85fb
manuq4979 <76844037+manuq4979@users.noreply.github.com>
2025-01-14 19:06:00 +0300

    Update task.py

--- task.py
+++ task.py
@@ -301,7 +301,7 @@ class HabitTask(Task):
     # Вернет False если ещё рано.
     def check_reset_time(self):
         # Дни прибавляем к дате, чтобы посмотреть, пора ли уже повторять:
-        current_date = str(datetime.now().date())
+        current_date = str(time_localization_function.current_datetime.date())
         # добавляем указанное кол-во дней к дате:
         date = datetime.strptime(current_date, "%Y-%m-%d").date() # Ожидает строку с датой в формате г-м-д !
         res = date + timedelta(days=int(self.activation_time))
