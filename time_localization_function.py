0fc4cb05dc52f1ecab18bd7a52eec3b67c4b6f8b
manuq4979 <76844037+manuq4979@users.noreply.github.com>
2025-01-14 16:57:38 +0300

    Update time_localization_function.py

--- time_localization_function.py
+++ time_localization_function.py
@@ -1,7 +1,17 @@
 from datetime import datetime, timedelta
 
+# Доступные часовые пояса:
 MOSCOW = 3 # +3 и дата и время будут по Москве!
-time_zone = MOSCOW
+
+
+file = open("DataApp/time_zone.txt", "r", encoding='utf-8')
+time_zone = file.read()
+file.close()
+
 current_time = datetime.now() + timedelta(hours=time_zone)
-    
+
+def set_time_zone(new_time_zone: int):
+    file = open("DataApp/time_zone.txt", "w+", encoding='utf-8')
+    file.write(new_time_zone)
+    file.close()
 
