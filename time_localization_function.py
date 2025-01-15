cfe0739b739914c4a97b049650756bd0ce685d42
manuq4979 <76844037+manuq4979@users.noreply.github.com>
2025-01-15 03:13:17 +0300

    Update time_localization_function.py

--- time_localization_function.py
+++ time_localization_function.py
@@ -4,6 +4,7 @@ import os.path
 time_zone_file_path = "DataApp/time_zone.txt"
 # Доступные часовые пояса:
 MOSCOW = 3 # +3 и дата и время будут по Москве!
+UTC = 0
 
 def set_time_zone(new_time_zone: int):
     file = open(time_zone_file_path, "w+", encoding='utf-8')
